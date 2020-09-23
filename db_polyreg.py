83510c6bc474a607290f1e9a0ac40332f07ecb61
sakoya00 <65825897+sakoya00@users.noreply.github.com>
2020-09-23 18:11:01 -0500

    Create db_polyreg.py

---
+++ db_polyreg.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+from sklearn import model_selection
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from sklearn.datasets import load_diabetes
+
+diabetes_data= load_diabetes()
+print (diabetes_data.keys())
+data1 = pd.DataFrame(data= np.c_[diabetes_data['data'], diabetes_data['target']],
+                     columns= diabetes_data['feature_names'] + ['target'])
+predictors= data1.drop('target', axis=1).values
+target_df= data1['target'].values
+X_train, X_test, y_train, y_test= train_test_split(predictors, target_df,test_size=0.30, random_state=42)
+
+linreg = LinearRegression()
+linreg.fit(X_train, y_train)
+linreg_pred= linreg.predict(X_test)
+
+poly = PolynomialFeatures(degree = 2) 
+X_poly = poly.fit_transform(predictors) 
+  
+poly.fit(X_poly, target_df)
