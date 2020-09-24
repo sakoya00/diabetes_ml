86779dd0957e86ffd5414259297e1a5429feb90f
sakoya00 <65825897+sakoya00@users.noreply.github.com>
2020-09-24 10:41:35 -0500

    Update db_lasso.py

--- db_lasso.py
+++ db_lasso.py
@@ -8,8 +8,6 @@ from sklearn.linear_model import RidgeCV
 from sklearn.linear_model import Lasso
 from sklearn.linear_model import ElasticNet
 from sklearn.preprocessing import PolynomialFeatures
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
@@ -20,7 +18,7 @@ from math import sqrt
 from sklearn.datasets import load_diabetes
 
 diabetes_data= load_diabetes()
-print (diabetes_data.keys())
+#print (diabetes_data.keys())
 data1 = pd.DataFrame(data= np.c_[diabetes_data['data'], diabetes_data['target']],
                      columns= diabetes_data['feature_names'] + ['target'])
 predictors= data1.drop('target', axis=1).values
@@ -36,9 +34,14 @@ print(lasso_0_reg.best_params_)
 lasso_1_reg = Lasso(alpha=0.05)
 lasso_1_reg.fit(X_train, y_train) 
 
-lasso_pred_test= lasso_1_reg.predict(X_test)
-rmse_lasso= np.sqrt(mean_squared_error(y_test, lasso_pred_test))
-print(rmse_lasso)
+lasso_pred_train= lasso_1_reg.predict(X_train)
+print("Lasso Train RMSE: %.2f"
+      % np.sqrt(mean_squared_error(y_train, lasso_pred_train)))
+print("Lasso Train R^2 Score: %.2f"
+      % r2_score(y_train, lasso_pred_train))
 
-r2_lasso= lasso_1_reg.score(X_test, lasso_pred_test)
-print(r2_lasso)
+lasso_pred_test= lasso_1_reg.predict(X_test)
+print("Lasso Test RMSE: %.2f"
+      % np.sqrt(mean_squared_error(y_test, lasso_pred_test)))
+print("Lasso Test R^2 Score: %.2f"
+      % r2_score(y_test, lasso_pred_test))
