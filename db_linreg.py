f5a34a4f50c02351e9ccaabf697362d579cb8cd7
sakoya00 <65825897+sakoya00@users.noreply.github.com>
2020-09-24 10:31:14 -0500

    Update db_linreg.py

--- db_linreg.py
+++ db_linreg.py
@@ -6,8 +6,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 from sklearn.linear_model import Lasso
 from sklearn.linear_model import ElasticNet
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
